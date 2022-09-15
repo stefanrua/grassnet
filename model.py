bc2303a95e1994e9fa69c4476078aa19bfc368ed
Stefan Rua <stefan.rua@iki.fi>
2022-09-15 13:05:25 +0300

    Reorganize

--- model.py
+++ model.py
@@ -16,35 +16,26 @@ import signal
 import timm
 import torch
  
+batch_size = 16
+epochs = 5
+learning_rate = 0.001
+valsplit = 0.2
+weight_decay = 0.01
+
+imgdir = 'images/rgb/'
+labelfile = 'labels/dmy.csv'
+outdir = 'out/'
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+imgsize = 224
+
 def handler(signum, frame):
     res = input(' exit? y/n ')
     if res == 'y':
         if len(err_val) > 0: save_results()
         exit(1)
- 
+
 signal.signal(signal.SIGINT, handler)
-
-batch_size = 16
-epochs = 5
-imgsize = 224
-learning_rate = 0.001
-valsplit = 0.2
-weight_decay = 0.01
-
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-imgdir = 'images/rgb/'
-labelfile = 'labels/dmy.csv'
-outdir = 'out/'
-
-transform_train = torch.nn.Sequential(
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation((-180, 180)),
-        transforms.CenterCrop(imgsize),
-        )
-
-transform_val = torch.nn.Sequential(
-        transforms.CenterCrop(imgsize),
-        )
 
 def getfid(fname):
     return int(fname.split('_id_')[1].split('.')[0])
@@ -84,39 +75,6 @@ class GrassDataset(Dataset):
 def nrmse(true, pred):
     return mean_squared_error(true, pred) / np.mean(true)
 
-data_train = GrassDataset(labelfile, imgdir, transform_train)
-data_val = GrassDataset(labelfile, imgdir, transform_val)
-
-N = len(data_train)/100
-indices = list(range(N))
-split = int(N*valsplit)
-random.shuffle(indices)
-train_idx, val_idx = indices[split:], indices[:split]
-sampler_train = SubsetRandomSampler(train_idx)
-sampler_val = SubsetRandomSampler(val_idx)
-dataloader_train = DataLoader(data_train,
-        batch_size=batch_size,
-        sampler=sampler_train)
-dataloader_val = DataLoader(data_val,
-        batch_size=batch_size,
-        sampler=sampler_val)
-
-model = timm.create_model('vgg16_bn',
-        num_classes=1,
-        pretrained=True,
-        in_chans=3)
-model = model.to(device)
-criterion = nn.MSELoss()
-optimizer = torch.optim.AdamW(model.parameters(),
-        lr=learning_rate,
-        weight_decay=weight_decay)
-
-w_best = copy.deepcopy(model.state_dict())
-err_best = np.inf
-err_train = []
-err_val = []
-err = 0
-
 def save_results():
     # runid = largest runid in outdir + 1
     if not os.path.exists(outdir):
@@ -133,8 +91,9 @@ def save_results():
     err_df.to_csv(f'{rundir}nrmse.csv', index=False)
     torch.save(w_best, f'{rundir}w_best.pt')
 
-# train: bool
-def epoch_sub(train):
+def epoch(train):
+    global err_train, err_val
+
     if train: model.train()
     else: model.eval()
     labels_ep = []
@@ -158,15 +117,61 @@ def epoch_sub(train):
     else: err_val.append(err)
     phase = 'train' if train else 'val'
     print(f'{phase} nrmse: {err:.3}')
+    return err
 
+def train():
+    global w_best, err_best
+    print('training...')
+    for e in range(epochs):
+        print(f'epoch {e+1}/{epochs}')
+        epoch(train=True)
+        err = epoch(train=False)
+        if err < err_best:
+            err_best = err
+            w_best = copy.deepcopy(model.state_dict())
 
-print('training...')
-for epoch in range(epochs):
-    print(f'epoch {epoch+1}/{epochs}')
-    epoch_sub(train=True)
-    epoch_sub(train=False)
-    if err < err_best:
-        err_best = err
-        w_best = copy.deepcopy(model.state_dict())
+# data
+transform_train = torch.nn.Sequential(
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation((-180, 180)),
+        transforms.CenterCrop(imgsize),
+        )
 
+transform_val = torch.nn.Sequential(
+        transforms.CenterCrop(imgsize),
+        )
+data_train = GrassDataset(labelfile, imgdir, transform_train)
+data_val = GrassDataset(labelfile, imgdir, transform_val)
+N = len(data_train)
+indices = list(range(N))
+split = int(N*valsplit)
+random.shuffle(indices)
+train_idx, val_idx = indices[split:], indices[:split]
+sampler_train = SubsetRandomSampler(train_idx)
+sampler_val = SubsetRandomSampler(val_idx)
+dataloader_train = DataLoader(data_train,
+        batch_size=batch_size,
+        sampler=sampler_train)
+dataloader_val = DataLoader(data_val,
+        batch_size=batch_size,
+        sampler=sampler_val)
+
+# model
+model = timm.create_model('vgg16_bn',
+        num_classes=1,
+        pretrained=True,
+        in_chans=3)
+model = model.to(device)
+criterion = nn.MSELoss()
+optimizer = torch.optim.AdamW(model.parameters(),
+        lr=learning_rate,
+        weight_decay=weight_decay)
+
+# results
+w_best = copy.deepcopy(model.state_dict())
+err_best = np.inf
+err_train = []
+err_val = []
+
+train()
 save_results()
